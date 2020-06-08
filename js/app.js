8f5581ae0f3ef8fcf25a343ed247265dc5642186
Ereyomi Oluwaseyi Samuel <ereyomioluwaseyi@gmail.com>
2020-06-08 21:18:26 +0100

    refactor: app js

--- js/app.js
+++ js/app.js
@@ -13,35 +13,41 @@ ereBtn.addEventListener( 'click', event => {
     navMenu.classList.toggle( "showNav" );
     console.log(navMenu.scrollHeight)
 } )
-window.onscroll = () => {
+
+
+//Change navigation style on scroll
+window.addEventListener( 'scroll', event => {
+    let nav = document.querySelector( '.nav-container' );
+
+    //( window.scrollY >= 44 ) ? nav.classList.add( 'scroll' ) : nav.classList.remove( 'scroll' );
+
     if ( document.body.scrollTop > 80 || document.documentElement.scrollTop > 80 )
     {
         header.style.background = 'white';
     } else
     {
         header.style.background = 'none';
-     }
-
-};
-
-/*  */
-window.addEventListener('DOMContentLoaded', () => {
-
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            const id = entry.target.getAttribute( 'id' );
-            console.log(id)
-            if (entry.intersectionRatio > 0) {
-                document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.add('active');
-            } else {
-                document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.remove('active');
-            }
-        });
-    });
-
-    // Track all sections that have an `id` applied
-    document.querySelectorAll('section[id]').forEach((section) => {
-        observer.observe(section);
-    });
+    }
     
-});
+} ); 
+
+//Active navigation on scroll
+window.addEventListener( 'scroll', event => {
+    let navigationLinks = document.querySelectorAll( '.nav-menu ul li a' );
+    let fromTop = window.scrollY;
+
+    navigationLinks.forEach( link => {
+        let section = document.querySelector( link.hash );
+        console.log(section)
+        if (
+            section.offsetTop <= fromTop &&
+            section.offsetTop + section.offsetHeight > fromTop
+        )
+        {
+            link.classList.add( 'active' );
+        } else
+        {
+            link.classList.remove( 'active' );
+        }
+    } );
+} );
